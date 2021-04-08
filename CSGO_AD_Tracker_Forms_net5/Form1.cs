2e43fa3bcf5f148e72ece5040978b6090893f20a
Cobular <22972550+JakeCover@users.noreply.github.com>
2021-04-07 17:08:11 -0700

    Mouse Movement Rate Tests

---
+++ CSGO_AD_Tracker_Forms_net5/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+
+namespace CSGO_AD_Tracker_Forms_net5
+{
+    public partial class Form1 : Form
+    {
+        private IKeyboardMouseEvents _mGlobalHook;
+        private KeyboardData _keyboardData;
+        private Int32 mouseEvents = 0;
+        private System.Timers.Timer aTimer;
+
+
+        public Form1()
+        {
+            aTimer = new System.Timers.Timer(2000);
+            _keyboardData = KeyboardData.Instance;
+            Subscribe();
+            InitializeComponent();
+            FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Unsubscribe();
+        }
+
+
+        public void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            _mGlobalHook = Hook.GlobalEvents();
+
+            _mGlobalHook.KeyDown += OnKeyDown;
+            _mGlobalHook.KeyUp += OnKeyUp;
+            _mGlobalHook.MouseMove += HookManager_MouseMove;
+
+
+            // m_GlobalHook.KeyPress += HookManager_KeyPress;
+        }
+
+        private void Unsubscribe()
+        {
+            if (_mGlobalHook == null) return;
+            _mGlobalHook.KeyDown -= OnKeyDown;
+            _mGlobalHook.KeyUp -= OnKeyUp;
+            _mGlobalHook.MouseMove -= HookManager_MouseMove;
+        }
+
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            _keyboardData.UpdateKeyStatus((CsgoKeys) e.KeyCode, true);
+            Console.WriteLine($"KeyDown  \t\t [{string.Join(",", _keyboardData.GetKeyStatuses)}]\n");
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            _keyboardData.UpdateKeyStatus((CsgoKeys) e.KeyCode, false);
+            Console.WriteLine($"KeyUp  \t\t [{string.Join(",", _keyboardData.GetKeyStatuses)}]\n");
+        }
+
+        private void HookManager_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Console.WriteLine($"x={e.X:0000}; y={e.Y:0000}");
+            if (aTimer.Enabled == false)
+            {
+                Console.WriteLine("Starting Timer...");
+                SetTimer();
+            }
+
+            mouseEvents++;
+        }
+
+        private void SetTimer()
+        {
+            Console.WriteLine("StaringTimer");
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine($"{mouseEvents / 2}");
+            mouseEvents = 0;
+        }
+    }
+}
---
+++ CSGO_AD_Tracker_Forms_net5/KeyboardData.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CSGO_AD_Tracker_Forms_net5
+{
+    public enum CsgoKeys
+    {
+        W = Keys.W,
+        A = Keys.A,
+        S = Keys.S,
+        D = Keys.D,
+        LCtrl = Keys.LControlKey,
+        Space = Keys.Space,
+    }
+
+    public sealed class KeyboardData
+    {
+        private static readonly Lazy<KeyboardData>
+            Lazy = new(() => new KeyboardData());
+
+        public static KeyboardData Instance => Lazy.Value;
+
+        // The status of various keys. True means pressed.
+        // 0: w
+        // 1: a
+        // 2: s
+        // 3: d
+        // 4: ctrl
+        // 5: space
+        private readonly bool[] _keyboardStatus = new bool[6];
+
+        /// <summary>
+        /// Updates the keyboardStatus with the given key information.
+        /// </summary>
+        /// <param name="key">A Forms.Keys, must be w, a, s, d, ctrl, or space.</param>
+        /// <param name="pressed">A boolean representing if the key is pressed or not</param>
+        /// <returns>True if key is one of those, false otherwise</returns>
+        public bool UpdateKeyStatus(CsgoKeys key, bool pressed)
+        {
+            switch (key)
+            {
+                case CsgoKeys.W:
+                    _keyboardStatus[0] = pressed;
+                    break;
+                case CsgoKeys.A:
+                    _keyboardStatus[1] = pressed;
+                    break;
+                case CsgoKeys.S:
+                    _keyboardStatus[2] = pressed;
+                    break;
+                case CsgoKeys.D:
+                    _keyboardStatus[3] = pressed;
+                    break;
+                case CsgoKeys.LCtrl:
+                    _keyboardStatus[4] = pressed;
+                    break;
+                case CsgoKeys.Space:
+                    _keyboardStatus[5] = pressed;
+                    break;
+                default:
+                    return false;
+            }
+
+            return true;
+        }
+
+        public bool CheckKeyStatus(CsgoKeys key)
+        {
+            switch (key)
+            {
+                case CsgoKeys.W:
+                    return _keyboardStatus[0];
+                case CsgoKeys.A:
+                    return _keyboardStatus[0];
+                case CsgoKeys.S:
+                    return _keyboardStatus[0];
+                case CsgoKeys.D:
+                    return _keyboardStatus[0];
+                case CsgoKeys.LCtrl:
+                    return _keyboardStatus[0];
+                case CsgoKeys.Space:
+                    return _keyboardStatus[0];
+                default:
+                    return false;
+            }
+        }
+
+        public bool[] GetKeyStatuses => _keyboardStatus;
+
+        private KeyboardData()
+        {
+        }
+    }
+}
